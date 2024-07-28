649180762a07881fefcd445a5f72a4e2513a56bc
TIBO <thibault.marie.pro@gmail.com>
2024-07-28 23:44:36 +0200

    WIP: add test folder to write script installer & packages list file

---
+++ ecpgpp-resonance/test/package-tool.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#TODO: display menu to user, info
+
+#TODO: READ toml file to parse, passed as argument
+
+#TODO: CHECK conformity of toml file
+
+#TODO: GET packages for each package manager
+
+#TODO: ASK user if they want to install packages
+
+#TODO: INSTALL packages with each package manager
